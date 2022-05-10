fba645535d50ffa17f83741a1e5bf9808f853879
duckbillLvr <86183856+duckbillLvr@users.noreply.github.com>
2022-05-10 15:09:57 +0900

    spring codeKing example get, post msg

---
+++ UMC_SpringBoot/ZenoTest/src/main/java/spring2/zenotest/ZenoTestApplication.java
@@ -0,0 +1,13 @@
+package spring2.zenotest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZenoTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZenoTestApplication.class, args);
+    }
+
+}
---
+++ UMC_SpringBoot/ZenoTest/src/test/java/spring2/zenotest/ZenoTestApplicationTests.java
@@ -0,0 +1,13 @@
+package spring2.zenotest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZenoTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
