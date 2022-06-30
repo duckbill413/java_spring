65fda7a970107b4a292355a8ec0a5d8de5c72efa
duckbillLvr <86183856+duckbillLvr@users.noreply.github.com>
2022-06-30 16:45:37 +0900

    Spring Instagram Clone Coding Update

--- UMC_SpringBoot/ZenoInstagram/ZenoInstagram/src/main/java/spring2/zenoinstagram/src/auth/AuthDao.java
+++ UMC_SpringBoot/ZenoInstagram/ZenoInstagram/src/main/java/spring2/zenoinstagram/src/auth/AuthDao.java
@@ -1,4 +1,32 @@
 package spring2.zenoinstagram.src.auth;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+import spring2.zenoinstagram.src.auth.model.*;
+import spring2.zenoinstagram.src.post.model.GetPostImgRes;
+
+import javax.sql.DataSource;
+
+@Repository
 public class AuthDao {
+    private JdbcTemplate jdbcTemplate;
+
+    @Autowired
+    public void setDataSource(DataSource dataSource) { this.jdbcTemplate = new JdbcTemplate(dataSource);}
+
+    public User getPwd(PostLoginReq postLoginReq){
+        String getPwdQuery = "SELECT userIdx, name, nickName, email, pwd from User wher email = ?";
+        String getPwdParams = postLoginReq.getEmail();
+
+        return this.jdbcTemplate.queryForObject(getPwdQuery,
+                (rs, rowNum) -> new User(
+                        rs.getInt("userIdx"),
+                        rs.getString("name"),
+                        rs.getString("nickName"),
+                        rs.getString("email"),
+                        rs.getString("pwd")
+                ),
+                getPwdParams);
+    }
 }
